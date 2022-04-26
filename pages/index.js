1268bfddf2a5afc5fdbfb2eea19ef49b252dbbf9
alexstouffer <bizkite@hotmail.com>
2022-04-26 16:37:48 -0700

    add sanity client/image-url, components for productItem, index gets list.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import { Alert, CircularProgress, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import Layout from '../components/Layout';
-import client from '../utils/Client';
+import ProductItem from '../components/ProductItem';
+import client from '../utils/client';
 
 export default function Home() {
   const [state, setState] = useState({
@@ -14,25 +15,29 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const products = await client.fetch(`*[_type == "Product"]`);
-        setState({products, loading: false});
+        const products = await client.fetch(`*[_type == "product"]`);
+        setState({ products, loading: false });
       } catch (err) {
         setState({ loading: false, error: err.message });
       }
     };
     fetchData();
   }, []);
-  return <Layout>
-    {loading? (<CircularProgress />)
-    : error? (<Alert variant="danger">{error}</Alert> )
-    :
-    <Grid container spacing={3}>
-      {products.map((product) => (
-        <Grid item md={4} key={product.slug}>
-          <Typography>{product.name}</Typography>
+  return (
+    <Layout>
+      {loading ? (
+        <CircularProgress />
+      ) : error ? (
+        <Alert variant="danger">{error}</Alert>
+      ) : (
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.slug}>
+              <ProductItem product={product}></ProductItem>
+            </Grid>
+          ))}
         </Grid>
-      ))}
-    </Grid>
-    }
-  </Layout>;
+      )}
+    </Layout>
+  );
 }
